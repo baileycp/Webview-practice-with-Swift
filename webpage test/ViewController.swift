a875e97a7154c64eaca96870aa25cf998f6c0b65
Bailey Peticolas <bailey.peticolas@principia.edu>
2020-05-30 02:12:12 -0500

    Initial Commit

---
+++ webpage test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webpage test
+//
+//  Created by Bailey Peticolas on 5/30/20.
+//  Copyright © 2020 Bailey Peticolas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ webpage testTests/webpage_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  webpage_testTests.swift
+//  webpage testTests
+//
+//  Created by Bailey Peticolas on 5/30/20.
+//  Copyright © 2020 Bailey Peticolas. All rights reserved.
+//
+
+import XCTest
+@testable import webpage_test
+
+class webpage_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
